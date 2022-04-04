2f2258a5de0272ecdc42ce9af11fc8bf16f7f9d6
vilasmathpati <84265150+vilasmathpati@users.noreply.github.com>
2022-04-04 21:54:42 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ if (close) {
   });
 }
 
-document.getElementById("hero").addEventListener("click", () => {
-  navbar.classList.remove("show");
+let products = document.querySelectorAll(".products");
+
+let mainimg = document.getElementById("bigimg");
+products.forEach((item) => {
+  item.addEventListener("click", () => {
+    window.location.href = "singlePro.html";
+  });
 });
+
+let Singleproimgs = document.getElementsByClassName("small-imgs");
+
+Singleproimgs[0].onclick = () => {
+  mainimg.src = Singleproimgs[0].src;
+};
+
+Singleproimgs[1].onclick = () => {
+  mainimg.src = Singleproimgs[1].src;
+};
+Singleproimgs[2].onclick = () => {
+  mainimg.src = Singleproimgs[2].src;
+};
+Singleproimgs[3].onclick = () => {
+  mainimg.src = Singleproimgs[3].src;
+};
